35aae388a08b9dbc9b679db871c252d297d9b50a
John Tönnäng <john_tonnang@hotmail.com>
2023-07-03 15:58:18 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex flex-col h-screen items-center w-full justify-center">
+      <button className="text-5xl m-2">Create game</button>
+      <button className="text-5xl m-2">Join game</button>
+      <button className="text-5xl m-2">Instructions</button>
+    </div>
+  )
+}
+
+export default App
